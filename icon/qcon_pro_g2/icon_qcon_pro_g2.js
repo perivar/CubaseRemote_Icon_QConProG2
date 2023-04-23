ed2fb7f92a5f37d7938b5f514865f1e25e86a0bf
Per Ivar Nerseth <perivar@nerseth.com>
2023-04-23 21:28:52 +0200

    First Test Version

--- icon/qcon_pro_g2/icon_qcon_pro_g2.js
+++ icon/qcon_pro_g2/icon_qcon_pro_g2.js
@@ -29,7 +29,10 @@ deviceDriver.makeDetectionUnit().detectPortPair(midiInput, midiOutput)
 
 var knobs = []
 var faders = []
-var buttons = []
+var sel_buttons = []
+var mute_buttons = []
+var solo_buttons = []
+var rec_buttons = []
 
 var numChannels = 8
 
@@ -57,6 +60,7 @@ for(var channelIndex = 0; channelIndex < numChannels; ++channelIndex) {
 
     knobs.push(knob)
 
+    // Fader
     var fader = deviceDriver.mSurface.makeFader(channelIndex * 2 + 0.5, 2, 1, 6)
 
     fader.mSurfaceValue.mOnTitleChange = (function (activeDevice, objectTitle, valueTitle) {
@@ -74,13 +78,67 @@ for(var channelIndex = 0; channelIndex < numChannels; ++channelIndex) {
         
     faders.push(fader) 
 
-    var button = deviceDriver.mSurface.makeButton(channelIndex * 2, 8, 2, 1)
+    // Channel Buttons
+    var sel_button = deviceDriver.mSurface.makeButton( channelIndex * 2, 6, 3, 3)
+    var mute_button = deviceDriver.mSurface.makeButton( channelIndex * 2, 9, 3, 3)
+    var solo_button = deviceDriver.mSurface.makeButton( channelIndex * 2, 12, 3, 3)
+    var rec_button = deviceDriver.mSurface.makeButton( channelIndex * 2, 15, 3, 3)
 
-    button.mSurfaceValue.mMidiBinding
-        .setInputPort(midiInput).setOutputPort(midiOutput)
+    rec_button.setShapeCircle()
+  
+    sel_button.mSurfaceValue.mMidiBinding
+        .setInputPort(midiInput)
         .bindToNote(0, 24 + channelIndex)
+    mute_button.mSurfaceValue.mMidiBinding
+        .setInputPort(midiInput)
+        .bindToNote(0, 16 + channelIndex)
+    solo_button.mSurfaceValue.mMidiBinding
+        .setInputPort(midiInput)
+        .bindToNote(0, 8 + channelIndex)
+    rec_button.mSurfaceValue.mMidiBinding
+        .setInputPort(midiInput)
+        .bindToNote(0, 0 + channelIndex)
 
-    buttons.push(button)
+    sel_button.mSurfaceValue.mOnProcessValueChange = (function (/** @type {MR_ActiveDevice} */activeDevice) {
+        console.log("SelButton ProcessValue Change:" + sel_button.mSurfaceValue.getProcessValue(activeDevice))
+        if (sel_button.mSurfaceValue.getProcessValue(activeDevice) > 0)
+            this.midiOutput.sendMidi(activeDevice, [0x90, 24 + channelIndex, 127])
+        else {
+            this.midiOutput.sendMidi(activeDevice, [0x90, 24 + channelIndex, 0])
+        }
+    }).bind({ midiOutput })
+
+    mute_button.mSurfaceValue.mOnProcessValueChange = (function (/** @type {MR_ActiveDevice} */activeDevice) {
+        console.log("MuteButton ProcessValue Change:" + mute_button.mSurfaceValue.getProcessValue(activeDevice))
+        if (mute_button.mSurfaceValue.getProcessValue(activeDevice) > 0)
+            this.midiOutput.sendMidi(activeDevice, [0x90, 16 + channelIndex, 127])
+        else {
+            this.midiOutput.sendMidi(activeDevice, [0x90, 16 + channelIndex, 0])
+        }
+    }).bind({ midiOutput })
+    
+    solo_button.mSurfaceValue.mOnProcessValueChange = (function (/** @type {MR_ActiveDevice} */activeDevice) {
+        console.log("SoloButton ProcessValue Change:" + solo_button.mSurfaceValue.getProcessValue(activeDevice))
+        if (solo_button.mSurfaceValue.getProcessValue(activeDevice) > 0)
+            this.midiOutput.sendMidi(activeDevice, [0x90, 8 + channelIndex, 127])
+        else {
+            this.midiOutput.sendMidi(activeDevice, [0x90, 8 + channelIndex, 0])
+        }
+    }).bind({ midiOutput })
+    
+    rec_button.mSurfaceValue.mOnProcessValueChange = (function (/** @type {MR_ActiveDevice} */activeDevice) {
+        console.log("RecButton ProcessValue Change:" + rec_button.mSurfaceValue.getProcessValue(activeDevice))
+        if (rec_button.mSurfaceValue.getProcessValue(activeDevice) > 0)
+            this.midiOutput.sendMidi(activeDevice, [0x90, 0 + channelIndex, 127])
+        else {
+            this.midiOutput.sendMidi(activeDevice, [0x90, 0 + channelIndex, 0])
+        }
+    }).bind({ midiOutput })    
+
+    sel_buttons.push(sel_button)
+    mute_buttons.push(mute_button)
+    solo_buttons.push(solo_button)
+    rec_buttons.push(rec_button)
 }
 
 //-----------------------------------------------------------------------------
@@ -96,11 +154,19 @@ var hostMixerBankZone = page.mHostAccess.mMixConsole.makeMixerBankZone()
 for(var channelIndex = 0; channelIndex < numChannels; ++channelIndex) {
     var hostMixerBankChannel = hostMixerBankZone.makeMixerBankChannel()
 
-    // var knobSurfaceValue = knobs[channelIndex].mSurfaceValue;
+    var knobSurfaceValue = knobs[channelIndex].mEncoderValue;
+    var knobPushValue = knobs[channelIndex].mPushValue;
     var faderSurfaceValue = faders[channelIndex].mSurfaceValue;
-    var buttonSurfaceValue = buttons[channelIndex].mSurfaceValue;
+    var sel_buttonSurfaceValue = sel_buttons[channelIndex].mSurfaceValue;
+    var mute_buttonSurfaceValue = mute_buttons[channelIndex].mSurfaceValue;
+    var solo_buttonSurfaceValue = solo_buttons[channelIndex].mSurfaceValue;
+    var rec_buttonSurfaceValue = rec_buttons[channelIndex].mSurfaceValue;
 
-    // page.makeValueBinding(knobSurfaceValue, hostMixerBankChannel.mValue.mPan)
-    page.makeValueBinding(faderSurfaceValue, hostMixerBankChannel.mValue.mVolume)
-    page.makeValueBinding(buttonSurfaceValue, hostMixerBankChannel.mValue.mSelected)
+    page.makeValueBinding(knobSurfaceValue, hostMixerBankChannel.mValue.mPan)
+    page.makeValueBinding(knobPushValue, hostMixerBankChannel.mValue.mEditorOpen).setTypeToggle()
+    page.makeValueBinding(faderSurfaceValue, hostMixerBankChannel.mValue.mVolume).setValueTakeOverModeJump()
+    page.makeValueBinding(sel_buttonSurfaceValue, hostMixerBankChannel.mValue.mSelected).setTypeToggle()
+    page.makeValueBinding(mute_buttonSurfaceValue, hostMixerBankChannel.mValue.mMute).setTypeToggle()
+    page.makeValueBinding(solo_buttonSurfaceValue, hostMixerBankChannel.mValue.mSolo).setTypeToggle()
+    page.makeValueBinding(rec_buttonSurfaceValue, hostMixerBankChannel.mValue.mRecordEnable).setTypeToggle()
 }
